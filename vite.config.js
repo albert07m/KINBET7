edaa4559ffeb2c24db6d85c66a0635afce3ad5f2
albert07m <92549671+albert07m@users.noreply.github.com>
2026-07-01 05:50:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Esto le dice a Vite que asegure que el punto de entrada es correcto
-      input: 'index.html',
-    },
-  },
 })
