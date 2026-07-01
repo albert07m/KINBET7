d0d08c6bc548b0cad6ca448be8b1b9a0ff522dea
albert07m <92549671+albert07m@users.noreply.github.com>
2026-07-01 05:32:46 +0200

    Add Rollup options for build configuration

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Esto le dice a Vite que asegure que el punto de entrada es correcto
+      input: 'index.html',
+    },
+  },
 })
